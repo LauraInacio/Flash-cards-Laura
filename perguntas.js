b357cee207e8acc3405fd54888625f4d2da3b71d
LauraInacio <145573341+LauraInacio@users.noreply.github.com>
2024-10-18 12:27:30 -0300

    18/10

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia', 
+    'Qual é o maior país em extensão territorial do mundo?',
+    'Rússia',
+)
+
+criaCartao(
+    'Ciências', 
+    'Qual é o planeta mais próximo do Sol?',
+    'Mercúrio',
+)
+
+criaCartao(
+    'LIteratura', 
+    'Quem escreveu o Dom Casmurro?',
+    'Machado de Assis',
+)
+
+criaCartao(
+    'Arte', 
+    'Qual artista pintou "A Noite EStrelada?',
+    'Vincent van Gogh',
+)
